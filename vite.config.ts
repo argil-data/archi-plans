a76a41709065b005ee0d346243d9915116e7cde2
Vincent Delaleu <vincent@argil-data.agency>
2024-07-26 14:53:50 +0200

    initial test preact

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [svelte()],
+  plugins: [preact()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
