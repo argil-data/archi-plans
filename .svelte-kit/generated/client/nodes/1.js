2bb05bb48f08b79d7fe4a3a8247839895e0a2de4
Vincent Delaleu <vincent@argil-data.agency>
2024-11-01 16:51:24 +0100

    update: config files

---
+++ .svelte-kit/generated/client/nodes/1.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.5.18_@sveltejs+vite-plugin-svelte@3.1.1_svelte@5.0.0-next.199_vite@5.3.5__sve_z4zusozbnmanhun3sgowlhe664/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,34 @@
 import { defineConfig } from "vite";
+import path from "path";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [svelte()],
+	plugins: [
+		svelte({
+			// hot: true, // Disable HMR for debugging
+		}),
+	],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-  },
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	build: {
+		target: "esnext", // Ensure Tauri and Vite use modern JS
+	},
+	server: {
+		port: 1420,
+		strictPort: true,
+		watch: {
+			// 3. tell vite to ignore watching `src-tauri`
+			ignored: ["**/src-tauri/**"],
+		},
+	},
+	resolve: {
+		alias: {
+			$lib: path.resolve("./src/lib"),
+		},
+	},
 }));
