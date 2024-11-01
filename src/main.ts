be96b84b780124127684343df8763d11a4c0a8b5
Vincent Delaleu <vincent@argil-data.agency>
2024-11-01 23:10:29 +0100

    update: script

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import "./styles.css";
+import "./shad.css";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+	target: document.getElementById("app"),
 });
 
 export default app;
